8aff1d000dba15cc59c31b154d0e4f80df280adf
Cole J Keller <colejkeller7@gmail.com>
2026-05-30 12:15:43 -0500

    type hints for tree class

--- Tree.py
+++ Tree.py
@@ -1,6 +1,9 @@
 class Tree:
-    def __init__(self, head):
+    def __init__(self, head) -> None:
         self.head = head
 
-    def __str__(self):
+    def __str__(self) -> str:
+        pass
+
+    def add(self, node) -> None:
         pass
